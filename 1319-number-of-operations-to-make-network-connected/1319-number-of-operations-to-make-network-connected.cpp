578ec663e215282813d9ac6052df8877f3521aed
Khushi Yadav <ky0307005@gmail.com>
2026-03-19 17:57:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (83%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited){
-        visited[node] = 1;
-        for(int n : adj[node]){
-            if(!visited[n]) dfs(n, adj, visited);
-        }
+    int find(int node, vector<int>& parent){
+        if(node == parent[node]) return node;
+
+        return parent[node] = find(parent[node], parent);
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
+        vector<int> parent(n);
+        vector<int> size(n, 1);
+        for(int i = 0; i<n; i++){
+            parent[i] = i;
+        }
+
+        int components = n;
+
         if(connections.size() < n-1) return -1;
 
-        vector<vector<int>> adj(n);
-        for(int i =0; i<connections.size(); i++){
+        for(int i = 0; i < connections.size(); i++){
             int u = connections[i][0];
             int v = connections[i][1];
+            int pu = find(u, parent);
+            int pv = find(v, parent);
 
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        int components = 0;
-        vector<int> visited(n, 0);
-        for(int i = 0; i<n; i++){
-            if(!visited[i]){
-                components++;
-                dfs(i, adj, visited);
+            if(pu == pv) continue;
+            if(size[pu] < size[pv]){
+                parent[pu] = pv;
+                size[pv] += size[pu];
+                components--;
+            } else {
+                parent[pv] = pu;
+                size[pu] += size[pv];
+                components--;
             }
         }
-        return components - 1;
+
+        return components-1;
     }
 };
