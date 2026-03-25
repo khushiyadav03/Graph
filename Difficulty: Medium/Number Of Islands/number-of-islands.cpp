7561b3eecca51266b9ddeac7017d3dd70fd5f0f9
Khushi Yadav <ky0307005@gmail.com>
2026-03-26 01:07:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number Of Islands/number-of-islands.cpp
+++ Difficulty: Medium/Number Of Islands/number-of-islands.cpp
@@ -1,25 +1,26 @@
 class DSU {
-public:
+    public :
     vector<int> parent, size;
-
-    DSU(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        for(int i = 0; i < n; i++) parent[i] = i;
+    DSU(int n){
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        
+        for(int i = 0; i<n; i++) parent[i] = i;
     }
-
-    int find(int node) {
+    
+    int find(int node){
         if(node == parent[node]) return node;
-        return parent[node] = find(parent[node]); // path compression
+        
+        return parent[node] = find(parent[node]);
     }
-
-    void UnionBySize(int u, int v) {
+    
+    void UnionBySize(int u, int v){
         int pu = find(u);
         int pv = find(v);
-
+        
         if(pu == pv) return;
-
-        if(size[pu] < size[pv]) {
+        
+        if(size[pu] < size[pv]){
             parent[pu] = pv;
             size[pv] += size[pu];
         } else {
@@ -29,54 +30,48 @@ public:
     }
 };
 
+
 class Solution {
-public:
+  public:
     vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators) {
-
-        DSU dsu(n * m);
         vector<vector<int>> visited(n, vector<int>(m, 0));
-
+        DSU dsu(n*m);
         vector<int> ans;
         int count = 0;
-
-        vector<pair<int,int>> dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-
-        for(auto &op : operators) {
-            int r = op[0];
-            int c = op[1];
-
-            // if already land
-            if(visited[r][c] == 1) {
+        
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
+        
+        for(int i = 0; i<operators.size(); i++){
+            int r = operators[i][0];
+            int c = operators[i][1];
+            
+            if(visited[r][c] == 1){
                 ans.push_back(count);
                 continue;
             }
-
-            // mark land
+            
             visited[r][c] = 1;
             count++;
-
+            
             int node = r * m + c;
-
-            // check 4 directions
-            for(auto &d : dir) {
-                int newRow = r + d.first;
-                int newCol = c + d.second;
-
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m) {
-                    if(visited[newRow][newCol] == 1) {
-                        int adjNode = newRow * m + newCol;
-
-                        if(dsu.find(node) != dsu.find(adjNode)) {
-                            dsu.UnionBySize(node, adjNode);
-                            count--;
-                        }
+            
+            for(int k = 0; k<4; k++){
+                int nr = r + dx[k];
+                int nc = c + dy[k];
+                
+                if(nr >=0 && nr <n && nc >=0 && nc < m && visited[nr][nc] == 1){
+                    int adjNode = nr *m + nc;
+                    
+                    if(dsu.find(node) != dsu.find(adjNode)){
+                        dsu.UnionBySize(node, adjNode);
+                        count--;
                     }
                 }
             }
-
             ans.push_back(count);
+            
         }
-
         return ans;
     }
 };
